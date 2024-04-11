21b98a4d24706fcda527c9b905997558c9673206
HermitBroccoli <vovo.r@ya.ru>
2024-04-12 00:11:02 +1000

    edit

--- resources/js/main.js
+++ resources/js/main.js
@@ -60,141 +60,204 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // edit property
-    const editProperty = (id) => {
-        openModalProperty(id);
-    }
 
-    const propertyList = document.querySelector('#property-list'),
-        deleteBtn = propertyList.querySelectorAll('[data-delete]'),
-        editBtn = propertyList.querySelectorAll('[data-edit]');
+    const propertyList = document.querySelector('#property-list')
 
-    deleteBtn.forEach(element => {
-        element.addEventListener('click', async () => await deletelProperty(element.getAttribute('data-delete')))
-    });
+    if (propertyList) {
 
-    editBtn.forEach(element => {
-        element.addEventListener('click', async () => editProperty(element.getAttribute('data-edit')))
+        const editProperty = (id) => {
+            openModalProperty(id);
+        }
 
-    })
+        const deleteBtn = propertyList.querySelectorAll('[data-delete]'),
+            editBtn = propertyList.querySelectorAll('[data-edit]');
 
-    const getPropertyOne = async (id) => {
-        const { data } = await axios.get(`/materil/property/get/${id}`)
-            .catch(error => alert('Произошла ошибка!'))
+        deleteBtn.forEach(element => {
+            element.addEventListener('click', async () => await deletelProperty(element.getAttribute('data-delete')))
+        });
 
-        return data
-    }
-    const modal = document.querySelector('#modal-property-edit'),
-        close = document.querySelector('#modal-close')
-    // Добавляем новый обработчик события
-    close.addEventListener('click', closeModalHandler);
+        editBtn.forEach(element => {
+            element.addEventListener('click', async () => editProperty(element.getAttribute('data-edit')))
 
-    // Определяем функцию обработчика для закрытия модального окна
-    function closeModalHandler() {
-        modal.classList.toggle('hidden');
-    }
-
-    const openModalProperty = async (id) => {
-        const form = modal.querySelector('#property-form'),
-            save = modal.querySelector('#modal-save')
-
-        const input1 = form.querySelector('#unitname'),
-            input2 = form.querySelector('#datestart'),
-            input3 = form.querySelector('#cost'),
-            input4 = form.querySelector('#costyear'),
-            input5 = form.querySelector('#costafter'),
-            input6 = form.querySelector('#period'),
-            input7 = form.querySelector('#hallid')
-
-
-        modal.addEventListener('submit', async (e) => {
-            e.preventDefault();
-
-            const data = {
-                unitid: id,
-                unitname: input1.value,
-                datestart: input2.value,
-                cost: parseFloat(input3.value),
-                costyear: Number(input4.value),
-                costafter: Number(input5.value),
-                period: Number(input6.value),
-                hallid: Number(input7.value)
-            }
-
-            await axios.patch(`/materil/property/edit`, {
-                ...data
-            })
-                .then(res => {
-                    if (res.status == 200) {
-                        window.location.reload()
-                    }
-                })
-                .catch(error => alert('Произошла ошибка!'))
         })
 
-
-
-        const data = await getPropertyOne(id)
-
-        form.querySelector('#unitname').value = data[1]
-        form.querySelector('#datestart').value = data[2]
-        form.querySelector('#cost').value = data[3]
-        form.querySelector('#costyear').value = data[4]
-        form.querySelector('#costafter').value = data[5]
-        form.querySelector('#period').value = data[6]
-        form.querySelector('#hallid').value = data[7]
-
-        modal.classList.toggle('hidden')
-
-    }
-
-    const openModalPropertyCreate = async () => {
-        const form = modal.querySelector('#property-form'),
-            save = modal.querySelector('#modal-save')
-
-        const input1 = form.querySelector('#unitname'),
-            input2 = form.querySelector('#datestart'),
-            input3 = form.querySelector('#cost'),
-            input4 = form.querySelector('#costyear'),
-            input5 = form.querySelector('#costafter'),
-            input6 = form.querySelector('#period'),
-            input7 = form.querySelector('#hallid')
-
-
-        input1.value = ""
-        input2.value = ""
-        input3.value = ""
-        input4.value = ""
-        input5.value = ""
-        input6.value = ""
-        input7.value = ""
-
-        modal.addEventListener('submit', async (e) => {
-            e.preventDefault();
-
-            const data = {
-                unitname: input1.value,
-                datestart: input2.value,
-                cost: parseFloat(input3.value),
-                costyear: Number(input4.value),
-                costafter: Number(input5.value),
-                period: Number(input6.value),
-                hallid: Number(input7.value)
-            }
-
-            await axios.post(`/materil/property/create`, {
-                ...data
-            })
-                .then(res => {
-                    if (res.status == 200) {
-                        window.location.reload()
-                    }
-                })
+        const getPropertyOne = async (id) => {
+            const { data } = await axios.get(`/materil/property/get/${id}`)
                 .catch(error => alert('Произошла ошибка!'))
-        })
 
-        modal.classList.toggle('hidden')
+            return data
+        }
+        const modal = document.querySelector('#modal-property-edit'),
+            close = document.querySelector('#modal-close')
+        // Добавляем новый обработчик события
+        close.addEventListener('click', closeModalHandler);
 
+        // Определяем функцию обработчика для закрытия модального окна
+        function closeModalHandler() {
+            modal.classList.toggle('hidden');
+        }
+
+        const openModalProperty = async (id) => {
+            const form = modal.querySelector('#property-form'),
+                save = modal.querySelector('#modal-save')
+
+            const input1 = form.querySelector('#unitname'),
+                input2 = form.querySelector('#datestart'),
+                input3 = form.querySelector('#cost'),
+                input4 = form.querySelector('#costyear'),
+                input5 = form.querySelector('#costafter'),
+                input6 = form.querySelector('#period'),
+                input7 = form.querySelector('#hallid')
+
+
+            modal.addEventListener('submit', async (e) => {
+                e.preventDefault();
+
+                const data = {
+                    unitid: id,
+                    unitname: input1.value,
+                    datestart: input2.value,
+                    cost: parseFloat(input3.value),
+                    costyear: Number(input4.value),
+                    costafter: Number(input5.value),
+                    period: Number(input6.value),
+                    hallid: Number(input7.value)
+                }
+
+                await axios.patch(`/materil/property/edit`, {
+                    ...data
+                })
+                    .then(res => {
+                        if (res.status == 200) {
+                            window.location.reload()
+                        }
+                    })
+                    .catch(error => alert('Произошла ошибка!'))
+            })
+
+
+
+            const data = await getPropertyOne(id)
+
+            form.querySelector('#unitname').value = data[1]
+            form.querySelector('#datestart').value = data[2]
+            form.querySelector('#cost').value = data[3]
+            form.querySelector('#costyear').value = data[4]
+            form.querySelector('#costafter').value = data[5]
+            form.querySelector('#period').value = data[6]
+            form.querySelector('#hallid').value = data[7]
+
+            modal.classList.toggle('hidden')
+
+        }
+
+        const openModalPropertyCreate = async () => {
+            const form = modal.querySelector('#property-form'),
+                save = modal.querySelector('#modal-save')
+
+            const input1 = form.querySelector('#unitname'),
+                input2 = form.querySelector('#datestart'),
+                input3 = form.querySelector('#cost'),
+                input4 = form.querySelector('#costyear'),
+                input5 = form.querySelector('#costafter'),
+                input6 = form.querySelector('#period'),
+                input7 = form.querySelector('#hallid')
+
+
+            input1.value = ""
+            input2.value = ""
+            input3.value = ""
+            input4.value = ""
+            input5.value = ""
+            input6.value = ""
+            input7.value = ""
+
+            modal.addEventListener('submit', async (e) => {
+                e.preventDefault();
+
+                const data = {
+                    unitname: input1.value,
+                    datestart: input2.value,
+                    cost: parseFloat(input3.value),
+                    costyear: Number(input4.value),
+                    costafter: Number(input5.value),
+                    period: Number(input6.value),
+                    hallid: Number(input7.value)
+                }
+
+                await axios.post(`/materil/property/create`, {
+                    ...data
+                })
+                    .then(res => {
+                        if (res.status == 200) {
+                            window.location.reload()
+                        }
+                    })
+                    .catch(error => alert('Произошла ошибка!'))
+            })
+
+            modal.classList.toggle('hidden')
+
+        }
+
+        document.querySelector('#create-property').addEventListener('click', async () => openModalPropertyCreate())
     }
 
-    document.querySelector('#create-property').addEventListener('click', async () => openModalPropertyCreate())
+    // edit building
+    const buildHome = document.querySelector('#modal-building-edit')
+
+    if (buildHome) {
+        const list = document.querySelector('[data-list]'),
+            btnEdit = list.querySelectorAll('[data-edit]'),
+            btnDelete = list.querySelectorAll('[data-delete]')
+
+        const saveClose = buildHome.querySelector('#modal-save'),
+            closeBtn = buildHome.querySelector('#modal-close');
+
+        const input1 = buildHome.querySelector('#id_kadasrt'),
+            input2 = buildHome.querySelector('#buildingname'),
+            input3 = buildHome.querySelector('#land'),
+            input4 = buildHome.querySelector('#material'),
+            input5 = buildHome.querySelector('#wear'),
+            input6 = buildHome.querySelector('#flow'),
+            input7 = buildHome.querySelector('#comment')
+
+        const openModal = () => {
+            buildHome.classList.remove('hidden')
+        }
+
+        closeBtn.addEventListener('click', () => closeModal())
+
+        const deleteItem = async (id) => {
+            axios.delete('/materil/building/delete',
+            {
+                id_building: id
+            }
+        )
+        }
+
+        const closeModal = () => {
+            input1.value = ""
+            input2.value = ""
+            input3.value = ""
+            input4.value = ""
+            input5.value = ""
+            input6.value = ""
+            input7.value = ""
+            
+            buildHome.classList.add('hidden')
+        }
+
+        const openModalEdit = async (id) => {
+            openModal()
+        }
+
+        const openModalCreate = async () => {
+
+        }
+
+        btnEdit.forEach(element => {
+            element.addEventListener('click', async () => openModalEdit(element.getAttribute('data-edit')))
+        })
+    }
 })
